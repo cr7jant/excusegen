79f09e40ccab3db78542b3b623717d41be4e9d2e
cr7jant <143714947+cr7jant@users.noreply.github.com>
2023-09-12 23:45:31 +0000

    git commit -m "changes"

--- src/app.js
+++ src/app.js
@@ -6,6 +6,22 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let quien = who[Math.floor(Math.random() * 4)];
+  let accion = action[Math.floor(Math.random() * 4)];
+  let que = what[Math.floor(Math.random() * 3)];
+  let cuando = when[Math.floor(Math.random() * 5)];
+  const h1 = document.querySelector(".mt-5");
+  h1.textContent = quien + " " + accion + " " + que + " " + cuando;
+  h1.append;
 };
